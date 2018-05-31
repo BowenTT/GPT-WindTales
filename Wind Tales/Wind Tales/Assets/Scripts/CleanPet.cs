225eed6d0628f8481c34d8cfac90260cc5903c01
ricardo <r.duinkerken@student.fotys.nl>
2018-05-31 17:09:35 +0200

    add brush interaction and filth countdown

--- Wind Tales/Wind Tales/Assets/Scripts/CleanPet.cs
+++ Wind Tales/Wind Tales/Assets/Scripts/CleanPet.cs
@@ -4,6 +4,39 @@ using UnityEngine;
 
 public class CleanPet : MonoBehaviour
 {
+    public int filthyness;
+    private Vector3 oldPosition;
 
+    void OnCollisionStay2D(Collision2D col)
+    {
+        if (filthyness > 0)
+        {
+            Debug.Log("collision detected");
+            switch (col.gameObject.tag)
+            {
+                case "Cleaning":
+                    Debug.Log("collision on cleaning detected");
+                    if (detectMovement(col))
+                    {
+                        filthyness--;
+                        Debug.Log("filthyness is " + filthyness);
+                    }
+                    break;
+            }
+        }
+    }
+
+    bool detectMovement(Collision2D col)
+    {  
+        Debug.Log("movement called");
+        if (oldPosition != col.transform.position)
+        {
+            Debug.Log("movement differs");
+            oldPosition = col.transform.position;
+            return true;
+        }
+        else
+            return false;
+    }
 
 }
