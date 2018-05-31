64c32a2c113a524c76ae509fea01c266a87d0b7e
Bowen <bowen158@hotmail.com>
2018-05-31 14:59:01 +0200

    Ball + Generating walls
    There is now a ball that you can kick by clicking on it, when the scene loads it will automatically generate borders (walls) around the screen so the ball cannot fly outside the borders. This is still WIP

---
+++ Wind Tales/Wind Tales/Assets/Scripts/GameElements/Ball.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    private Rigidbody BallObject;
+    public int force = 1000;
+	// Use this for initialization
+	void Start () {
+        BallObject = gameObject.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	private void OnMouseDown()
+	{
+        Vector2 direction = new Vector2(Random.Range(-1, 1), Random.Range(-1, 1));
+        BallObject.AddForce(direction * force);
+	}
+}
