b53173cce70782cfae2107b5ca733ab9795a2768
ricardo <r.duinkerken@student.fotys.nl>
2018-05-31 18:38:24 +0200

    add filth animation

--- Wind Tales/Wind Tales/Assets/Scripts/CleanPet.cs
+++ Wind Tales/Wind Tales/Assets/Scripts/CleanPet.cs
@@ -1,11 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CleanPet : MonoBehaviour
 {
     public int filthyness;
+
+    public Sprite filth3;
+    public Sprite filth2;
+    public Sprite filth1;
+    public Sprite clean;
+
+    private int MaxFilth;
+    private GameObject currentPet;
     private Vector3 oldPosition;
+
+    void Start()
+    {
+        Debug.Log("set maxFilth");
+        MaxFilth = filthyness;
+        Debug.Log(MaxFilth);
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Debug.Log("set current gameobject");
+        currentPet = col.otherCollider.gameObject;
+    }
 
     void OnCollisionStay2D(Collision2D col)
     {
@@ -20,6 +42,7 @@ public class CleanPet : MonoBehaviour
                     {
                         filthyness--;
                         Debug.Log("filthyness is " + filthyness);
+                        UpdateSprite();
                     }
                     break;
             }
@@ -37,6 +60,19 @@ public class CleanPet : MonoBehaviour
         }
         else
             return false;
+    }
+
+    void UpdateSprite()
+    {
+        Image filthupdate = currentPet.GetComponent<Image>();
+        if (filthyness == 0)
+            filthupdate.sprite = clean;
+        if (filthyness == MaxFilth / 4)
+            filthupdate.sprite = filth1;
+        if (filthyness == MaxFilth / 3)
+            filthupdate.sprite = filth2;
+        if (filthyness == MaxFilth / 2)
+            filthupdate.sprite = filth3;
     }
 
 }
