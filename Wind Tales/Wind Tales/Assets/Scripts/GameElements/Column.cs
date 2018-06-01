fc5501ff97f89adc07598778c926d03082f1907e
Jesse <jesse0jr0@gmail.com>
2018-06-01 14:44:26 +0200

    50% werkende flappybird

---
+++ Wind Tales/Wind Tales/Assets/Scripts/GameElements/Column.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Column : MonoBehaviour
+{
+    public GameObject Blockage;
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.GetComponent<Character>() != null)
+		{
+			//If the bird hits the trigger collider in between the columns then
+			//tell the game control that the bird scored.
+			GameControl.instance.BirdScored();
+		}
+	}
+}
