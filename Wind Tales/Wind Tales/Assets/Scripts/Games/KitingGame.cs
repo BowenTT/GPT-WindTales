2f646367106571a470ebfe2b9008949849ce679f
Unknown <roelantvdweijden@gmail.com>
2018-06-01 12:08:29 +0200

    Added gravity to kiting game

--- Wind Tales/Wind Tales/Assets/Scripts/Games/KitingGame.cs
+++ Wind Tales/Wind Tales/Assets/Scripts/Games/KitingGame.cs
@@ -14,6 +14,109 @@ namespace Games
 	{
 		public float gravity;
 		public float airflow;
+		public float startingBoost;
+		public GameObject background;
+		public Transform startingPosition;
+		public Transform groundLevel;
+		public Transform upperLevel;
+		public GameObject victory;
+
+		private kiteGameState currentState = kiteGameState.starting;
+		private float points = 0;
+		private float velocity = 0;
+
+		void Start ()
+		{
+			background.transform.position = startingPosition.position;
+			victory.SetActive(false);
+		}
+	
+		void Update ()
+		{
+			float input = Input.GetAxis("Player_SimulateBreathing");
+			UpdateState(input);
+			UpdateGame(input);
+			points += input;
+		}
+
+		public override StatChange Play()
+		{
+			return new StatChange(0,0,0);
+		}
+
+		private void UpdateState(float input)
+		{
+
+			switch(currentState)
+			{
+				default:
+					//do nothing
+				break;
+
+				case kiteGameState.starting:
+					if (input > 0)
+					{
+						velocity = input*startingBoost;
+						currentState = kiteGameState.playing;
+					}
+				break;
+
+				case kiteGameState.playing:
+					if (velocity <= 0 || background.transform.position.y > groundLevel.position.y || background.transform.position.y < upperLevel.position.y)
+					{
+						velocity = 0;
+						gravity = gravity/10;
+						currentState = kiteGameState.finished;
+						victory.SetActive(true);
+					}
+				break;
+			}
+		}
+
+		private void UpdateGame(float input)
+		{
+			switch(currentState)
+			{
+				default:
+					//do nothing
+				break;
+					case kiteGameState.playing:
+					//effects of gravity
+					//background.transform.position += new Vector3(0, gravity, 0);
+					//effect of blowing
+					//background.transform.position -= new Vector3(0, airflow*input, 0);
+					velocity -= gravity;
+					velocity += airflow*input;
+					background.transform.position -= new Vector3(0, velocity, 0);
+				break;
+					case kiteGameState.finished:
+					if (background.transform.position.y < groundLevel.position.y)
+					{
+						//effects of gravity
+						velocity -= gravity;
+						background.transform.position -= new Vector3(0, velocity, 0);
+					}
+					else
+					{
+						background.transform.position = groundLevel.position;
+					}
+				break;
+			}
+		}
+	}
+}
+
+
+/*
+namespace Games
+{
+
+	enum kiteGameState {starting, playing, finished};
+
+	public class KitingGame : MiniGames
+	{
+		public float gravity;
+		public float airflow;
 		public GameObject background;
 		public Transform startingPosition;
 		public Transform groundLevel;
@@ -92,3 +195,4 @@ namespace Games
 		}
 	}
 }
+*/
