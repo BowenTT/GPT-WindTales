20c6bd0cb152c15aed6d37127a1aba57cdf6fd1a
FlorisFeddema <9294266+FlorisFeddema@users.noreply.github.com>
2018-06-15 10:23:05 +0200

    excryvtb

---
+++ Wind Tales/Wind Tales/Assets/BackgroundSound.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundSound : MonoBehaviour
+{
+
+
+    static BackgroundSound Instance;
+    public AudioSource source;
+    // Use this for initialization
+    void Start()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+}
--- Wind Tales/Wind Tales/Assets/Scripts/CleanPet.cs
+++ Wind Tales/Wind Tales/Assets/Scripts/CleanPet.cs
@@ -18,6 +18,7 @@ public class CleanPet : MonoBehaviour
     public Sprite wetOverlay2;
     public Sprite wetOverlay3;
     public Image wetOverlay;
+    public float minInput;
 
     private float maxFlow;
     private float MaxFilth;
@@ -42,9 +43,9 @@ public class CleanPet : MonoBehaviour
 
     void Update()
     {
-        float input = Input.GetAxis("Player_SimulateBreathing");
+        float input = DeviceManager.Instance.MinFlowLMin;
         Debug.Log(input);
-        if (input > 0 && !Measuring)
+        if (input > minInput && !Measuring)
         {
             StartCoroutine(measureMaxFlow());
         }
@@ -69,14 +70,14 @@ public class CleanPet : MonoBehaviour
         var start = DateTime.Now;
         while ((DateTime.Now - start).Seconds < 2)
         {
-            measures.Add(Input.GetAxis("Player_SimulateBreathing"));
+            measures.Add(DeviceManager.Instance.MinFlowLMin);
             yield return new WaitForSeconds(0.0005f);
         }
         foreach (var flow in measures)
         {
             if (flow > maxMeasuredFlow)
             {
-               maxMeasuredFlow = flow;
+                maxMeasuredFlow = flow;
             }
         }
         Debug.Log("max Measured flow " + maxMeasuredFlow);
@@ -84,7 +85,7 @@ public class CleanPet : MonoBehaviour
     }
 
 
-        void OnCollisionEnter2D(Collision2D col)
+    void OnCollisionEnter2D(Collision2D col)
     {
         Debug.Log("set current gameobject");
         currentPet = col.otherCollider.gameObject;
@@ -114,7 +115,7 @@ public class CleanPet : MonoBehaviour
     }
 
     bool detectMovement(Collision2D col)
-    {  
+    {
         Debug.Log("movement called");
         if (oldPosition != col.transform.position)
         {
@@ -173,7 +174,7 @@ public class CleanPet : MonoBehaviour
             wetOverlay.sprite = wetOverlay3;
         }
         loop++;
-        
+
         if (loop > 150)
         {
             loop = 0;
